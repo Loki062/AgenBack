4af2a0dfe22f17dd17c3e89269125646a2b54aee
LokiGoiano <lorddavi19@gmail.com>
2024-10-01 11:34:17 -0300

    att cors politica

--- build/server.js
+++ build/server.js
@@ -1,35 +1,33 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the API!");
+  res.send("Welcome to the API!");
 });
 
+app.use(express.json());
+app.use(cors());
 
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-const prisma = new client_1.PrismaClient();
-
-
-
-app.use(express_1.default.json());
-app.use((0, cors_1.default)());
 app.post("/create-appointments", async (req, res) => {
-    const { name, inital_date, final_Date, room } = req.body;
-    const appointments = await prisma.appointments.create({
-        data: {
-            name,
-            inital_date,
-            final_Date,
-            room,
-        },
-    });
-    res.json(appointments);
+  const { name, inital_date, final_Date, room } = req.body;
+
+  const appointments = await prisma.appointments.create({
+    data: {
+      name,
+      inital_date,
+      final_Date,
+      room,
+    },
+  });
+
+  res.json(appointments);
 });
+
 const port = 3333;
+
 app.listen(process.env.PORT || port, () => console.log("server listening on"));
