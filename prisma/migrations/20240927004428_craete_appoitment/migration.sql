09645154b868fc1058d3076e7c9c9fd7319d089d
LokiGoiano <lorddavi19@gmail.com>
2024-10-01 15:31:21 -0300

    atualizada migration

--- prisma/migrations/20240927004428_craete_appoitment/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Appointments" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "inital_date" DATETIME NOT NULL,
-    "final_Date" DATETIME NOT NULL,
-    "room" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
